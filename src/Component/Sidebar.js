4bb7a1be574af2ebb3189e3acf59cdcf392288af
ramangiri <60056285+ramangiri@users.noreply.github.com>
2021-06-29 20:41:47 +0530

    Update Sidebar.js

--- src/Component/Sidebar.js
+++ src/Component/Sidebar.js
@@ -5,12 +5,12 @@ export default function Sidebar(){
          <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     
-         <a className="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+         <div className="sidebar-brand d-flex align-items-center justify-content-center">
         <div className="sidebar-brand-icon rotate-n-15">
             <i className="fas fa-laugh-wink"></i>
         </div>
         <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-         </a>
+         </div>
 
     
     <hr className="sidebar-divider my-0"/>
